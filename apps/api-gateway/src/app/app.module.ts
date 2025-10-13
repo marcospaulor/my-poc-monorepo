a76cdddf6413f3267a06c13f4f578496f36d058e
marcospaulor <dev.silva.marcos@gmail.com>
2025-10-13 01:06:12 -0300

    feat: integrate DomainExceptionFilter for improved error handling in app module

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './companies/company.module';
+import { DomainExceptionFilter } from './filters/domain-exception.filter';
 
 @Module({
   imports: [CompanyModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: DomainExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
